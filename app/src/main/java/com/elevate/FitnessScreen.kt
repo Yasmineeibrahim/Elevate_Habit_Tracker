cd3cb3de9682151eb9aacab64755bfa16c8c87d2
Basmala49 <bosybob2005@gmail.com>
2025-04-20 17:38:18 +0200

    first update

--- app/src/main/java/com/elevate/FitnessScreen.kt
+++ app/src/main/java/com/elevate/FitnessScreen.kt
@@ -1,12 +1,22 @@
 package com.elevate
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,73 +25,96 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.elevate.ui.theme.Poppins
 
-
-@Composable
-fun FitnessScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Top
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.image), // ← تأكد إن الصورة موجودة في res/drawable
-            contentDescription = "Workout Image",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(280.dp)
-                .clip(RoundedCornerShape(24.dp)),
-            contentScale = ContentScale.Crop
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = "Push Your Limits,\nReach Your Goals.",
-            fontSize = 35.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Text(
-            text = "Turn your fitness dreams into reality! Set clear exercise goals, track your progress, and stay motivated every step of the way. Whether you're aiming for strength, endurance, or overall wellness, consistent effort leads to lasting results. Keep moving forward and achieve your best self.",
-            fontSize = 20.sp,
-            color = Color.Gray,
-            lineHeight = 30.sp
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Box(
-            modifier = Modifier
-                .size(48.dp)
-                .align(Alignment.End)
-                .clip(CircleShape)
-                .background(Color(0xFFA24D6A)),
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                imageVector = Icons.Default.ArrowForward,
-                contentDescription = "Next",
-                tint = Color.White
-            )
+class SleepActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                SleepScreen()
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun FitnessScreenPreview() {
-    MaterialTheme {
-        FitnessScreen()
+fun FitnessScreen(onNextClicked: () -> Unit = {}) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(horizontal = 24.dp, vertical = 16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.image),
+                contentDescription = "Drink Illustration",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+
+            )
+            Spacer(modifier = Modifier.height(25.dp))
+            Text(
+                text = "Push Your Limits,Reach Your Goals.",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                fontFamily = Poppins,
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+            Column (
+                modifier = Modifier.fillMaxWidth()
+            ){ Text(
+                text = "Turn your fitness dreams into reality! Set clear exercise goals, track your progress, and stay motivated every step of the way. Whether you're aiming for strength, endurance, or overall wellness, consistent effort leads to lasting results. Keep moving forward and achieve your best self.",
+                textAlign = TextAlign.Start,
+                style = TextStyle(fontSize = 16.sp,
+                    color = Color.DarkGray,
+                    fontFamily = Poppins,
+
+                    fontWeight = FontWeight.Normal
+                )
+            )
+            }  }
+
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.End
+        ) {
+            Spacer(modifier = Modifier.height(26.dp))
+
+            IconButton(
+                onClick = {
+                    val intent = Intent(context, TakeoffActivity::class.java)
+                    context.startActivity(intent)
+                },
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xFFE39EBF))
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_play),
+                    contentDescription = "Next",
+                    tint = Color.Black
+                )
+            }
+        }
     }
 }
