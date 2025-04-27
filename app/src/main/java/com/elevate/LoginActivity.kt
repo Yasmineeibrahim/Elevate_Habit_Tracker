8d45bab4dffa0e6cd796d3c16652d32d8e796339
Basmala49 <bosybob2005@gmail.com>
2025-04-27 23:25:50 +0300

    first update

--- app/src/main/java/com/elevate/LoginActivity.kt
+++ app/src/main/java/com/elevate/LoginActivity.kt
@@ -13,7 +13,6 @@ import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
